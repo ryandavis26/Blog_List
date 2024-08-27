71979f01e01b615ed35a2019a5ff1a59f4b91567
Ryan Davis <ryandavisim@gmail.com>
2024-08-27 16:10:22 -0400

    Reformatted project with express best practices
    Added new router and middleware to handle post and get requests for blog
    objects from the mongodb database

---
+++ app.js
@@ -0,0 +1,32 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogRouter = require('./controllers/bloglist')
+const middleware = require('./utils/middleware.js')
+const logger = require('./utils/logger.js')
+const mongoose = require('mongoose')
+
+
+mongoose.set('strictQuery', false)
+logger.info("connecting to the DB")
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('Connected to the DB')
+  })
+  .catch((error) => {
+    logger.error("error connecting MongoDb", error.message)
+  })
+
+app.use(cors())
+//app.use(express.static('dist'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,52 +1,8 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const logger = require('./utils/logger.js')
-const config = require('./utils/config.js')
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
+app.listen(config.PORT, () => {
+  logger.info(`Server is running on ${config.PORT}`)
 })
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = config.MONGODB_URI
-
-mongoose.connect(mongoUrl)
-  .then(() => {
-    logger.info('Connected to the DB')
-  })
-  .catch((error) => {
-    logger.error(error)
-  })
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = config.PORT
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
-})
