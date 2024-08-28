0cadc7650c3f6189c7d37c5e8907d32be9da7cda
Ryan Davis <ryandavisim@gmail.com>
2024-08-27 21:38:15 -0400

    Added 2 new helper functions to list helper
    Also added 2 tests to the tests dir for the list helper tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,4 +2,41 @@ const dummy = (blogs) => {
   return 1
 }
 
-module.exports = dummy
+//Takes in an array of blogs and gets the sum of the likes from all blogs
+const totalLikes = (blogs) => {
+  //return 0 likes if the array is empty
+  if (blogs.length === 0) {
+    return 0
+  }
+
+  //Sum over the array
+  const sumLikes = blogs.reduce((acc, curval) => acc + curval.likes, 0)
+  return sumLikes
+}
+
+
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return 0
+  }
+
+  let curMax = 0;
+  let indexOfCurMax = 0
+  let indexCounter = 0
+
+  for (const blog of blogs) {
+    if (blog.likes > curMax) {
+      curMax = blog.likes
+      indexOfCurMax = indexCounter
+    }
+    indexCounter++
+  }
+  return blogs[indexOfCurMax]
+}
+
+
+
+
+
+module.exports = { dummy, totalLikes, favoriteBlog }
