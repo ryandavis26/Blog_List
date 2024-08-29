0f31d414e07c622f2b7590c3caaab6c54f3d16f1
Ryan Davis <ryandavisim@gmail.com>
2024-08-29 18:17:34 -0400

    Refactored Controller with async/await format
    Added a test to post to ensure that a new blog can be added with the
    correct content

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const {test, after, beforeEach} = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
@@ -10,46 +10,67 @@ const Blog = require('../models/blog')
 
 
 beforeEach(async () => {
-    //Delete the existing Blogs from the test DB
-    await Blog.deleteMany({})
+  //Delete the existing Blogs from the test DB
+  await Blog.deleteMany({})
 
-    await Blog.insertMany(helper.initialBlogs)
+  await Blog.insertMany(helper.initialBlogs)
 })
 
 //Test using the a regex to see if the Blog List in the test directory returns a json formatted string
 test('blogs are returned as json', async () => {
-    await api
+  await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
 
 test('there exists only 6 blogs', async () => {
-    const response = await api.get('/api/blogs')
+  const response = await api.get('/api/blogs')
 
-    assert.strictEqual(response.body.length, 6)
+  assert.strictEqual(response.body.length, 6)
 })
 
-test.only('check for id field existence', async () => {
-    const response = await api.get('/api/blogs')
+test('check for id field existence', async () => {
+  const response = await api.get('/api/blogs')
 
-    //Body is and array of Blog Objects
-    const body = response.body
+  //Body is and array of Blog Objects
+  const body = response.body
 
-    console.log(response.body)
+  console.log(response.body)
 
-    let idFieldExists = true
+  let idFieldExists = true
 
-    body.forEach((blog) => {
-        if(!blog.id){
-            idFieldExists = false
-        }
-    })
+  body.forEach((blog) => {
+    if (!blog.id) {
+      idFieldExists = false
+    }
+  })
 
 
-    assert(idFieldExists)
+  assert(idFieldExists)
 })
+
+
+test.only('Insert a blog into the DB', async () => {
+  const newBlog = {
+    title: "JS is a weird langauge",
+    author: "Ryan Davis",
+    url: "ryandavisim@co.uk",
+    likes: 7
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)
+})
+
 
 after(async () => {
-    await mongoose.connection.close()
-  })
+  await mongoose.connection.close()
+})
